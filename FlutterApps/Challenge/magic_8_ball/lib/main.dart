60c13c2d00e37c587df00bfb20a74c7b98fb91e4
Jaspar Ifechi <j.cyxtus@gmail.com>
2020-10-21 23:55:55 +0000

    D21 8 Ball: Completed the basic functionality of the app

--- FlutterApps/Challenge/magic_8_ball/lib/main.dart
+++ FlutterApps/Challenge/magic_8_ball/lib/main.dart
@@ -1,11 +1,45 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Text('Home'),
+        backgroundColor: Colors.indigo,
+        appBar: AppBar(
+          backgroundColor: Colors.indigo.shade700,
+          title: Text(
+            'Ask me Anything!',
+            style: TextStyle(
+              fontFamily: 'Nunito',
+              fontSize: 25,
+            ),
+          ),
+        ),
+        body: Ball(),
       ),
     ),
   );
+}
+
+class Ball extends StatefulWidget {
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<Ball> {
+  int ballNum = 1;
+  Widget build(BuildContext context) => Center(
+        child: Container(
+          child: TextButton(
+            onPressed: () {
+              setState(() {
+                ballNum = Random().nextInt(5) + 1;
+              });
+            },
+            child: Expanded(
+              child: Image.asset('images/ball$ballNum.png'),
+            ),
+          ),
+        ),
+      );
 }

1b13423d5c98c42c38373aa89373e9aca2e92178
Jaspar Ifechi <j.cyxtus@gmail.com>
2020-10-21 03:14:16 +0000

    D20: Added fonts to the dice app and coded the dice page

--- FlutterApps/dicee-flutter/lib/main.dart
+++ FlutterApps/dicee-flutter/lib/main.dart
@@ -1,23 +1,49 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  return runApp(
-    MaterialApp(
+main() => runApp(MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.red.shade600,
+          toolbarHeight: 70,
+          title: Center(
+            child: Text(
+              "Dice App",
+              style: TextStyle(
+                color: Colors.red.shade50,
+                fontSize: 45,
+                fontFamily: 'Mulish-Black',
+                letterSpacing: 6,
+              ),
+            ),
+          ),
         ),
         body: DicePage(),
       ),
-    ),
-  );
-}
+    ));
 
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            // flex: 3,
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            // flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

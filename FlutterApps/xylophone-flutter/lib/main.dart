cab69714c4359c805fe1cd1c74d649384139e1de
Jaspar Ifechi <j.cyxtus@gmail.com>
2020-10-22 23:40:20 +0000

    D22 Xylophone app:added the basic functionality.

--- FlutterApps/xylophone-flutter/lib/main.dart
+++ FlutterApps/xylophone-flutter/lib/main.dart
@@ -1,14 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNum) {
+    final player = AudioCache();
+    player.play('note$soundNum.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.red),
+                  ),
+                  onPressed: () {
+                    playSound(1);
+                  },
+                  child: null,
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.teal),
+                  ),
+                  onPressed: () {
+                    playSound(2);
+                  },
+                  child: null,
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.purple),
+                  ),
+                  onPressed: () {
+                    playSound(3);
+                  },
+                  child: null,
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.orange),
+                  ),
+                  onPressed: () {
+                    playSound(4);
+                  },
+                  child: null,
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.blue),
+                  ),
+                  onPressed: () {
+                    playSound(5);
+                  },
+                  child: null,
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.yellow),
+                  ),
+                  onPressed: () {
+                    playSound(6);
+                  },
+                  child: null,
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.green),
+                  ),
+                  onPressed: () {
+                    playSound(7);
+                  },
+                  child: null,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

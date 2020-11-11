18310da61584e0adb1f67ab083da0d7810514c7f
Jaspar Ifechi <j.cyxtus@gmail.com>
2020-11-11 04:23:24 +0000

    D41-42 Flash Chat App: Integrated Firebase and implemented user registration.

--- FlutterApps/flash-chat-flutter/lib/screens/registration_screen.dart
+++ FlutterApps/flash-chat-flutter/lib/screens/registration_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flash_chat/components/rounded_btn.dart';
 import 'package:flash_chat/constants.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const String id = 'RegistrationScreen';
@@ -10,6 +13,10 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  String email;
+  String password;
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,8 +38,9 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 48.0,
             ),
             TextField(
+              textAlign: TextAlign.center,
               onChanged: (value) {
-                //Do something with the user input.
+                email = value;
               },
               decoration: kTextFieldDecoration.copyWith(
                 labelText: 'Enter your email',
@@ -42,8 +50,10 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 8.0,
             ),
             TextField(
+              textAlign: TextAlign.center,
+              obscureText: true,
               onChanged: (value) {
-                //Do something with the user input.
+                password = value;
               },
               decoration: kTextFieldDecoration.copyWith(
                 labelText: 'Enter your password',
@@ -53,8 +63,25 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 24.0,
             ),
             AppBtn(
-              onTap: () {
-                Navigator.pop(context);
+              onTap: () async {
+                try {
+                  final newUser = await _auth.createUserWithEmailAndPassword(
+                    email: email.trim(),
+                    password: password.trim(),
+                  );
+
+                  if (newUser != null) {
+                    Navigator.pushNamed(context, ChatScreen.id);
+                  }
+                } on FirebaseException catch (e) {
+                  // if(e.code == ){}
+                  Alert(
+                    context: context,
+                    title: "",
+                    desc: "${e.message}",
+                    // image: Image.asset("assets/success.png"),
+                  ).show();
+                }
               },
               btnColor: Colors.blueAccent,
               btnTitle: 'Register',

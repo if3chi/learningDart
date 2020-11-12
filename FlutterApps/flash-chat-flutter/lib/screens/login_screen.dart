c1c3e38d666902b44ff10d4af63091c7f7771c8d
Jaspar Ifechi <j.cyxtus@gmail.com>
2020-11-12 00:38:33 +0000

    D43bFlash Chat app: Implemented user login and signout

--- FlutterApps/flash-chat-flutter/lib/screens/login_screen.dart
+++ FlutterApps/flash-chat-flutter/lib/screens/login_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flash_chat/components/rounded_btn.dart';
 import 'package:flash_chat/constants.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = 'LoginScreen';
@@ -10,6 +13,10 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  String email;
+  String passwd;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,7 +41,7 @@ class _LoginScreenState extends State<LoginScreen> {
               textAlign: TextAlign.center,
               keyboardType: TextInputType.emailAddress,
               onChanged: (value) {
-                //Do something with the user input.
+                email = value;
               },
               decoration: kTextFieldDecoration.copyWith(
                 labelText: 'Enter your email',
@@ -47,7 +54,7 @@ class _LoginScreenState extends State<LoginScreen> {
               obscureText: true,
               textAlign: TextAlign.center,
               onChanged: (value) {
-                //Do something with the user input.
+                passwd = value;
               },
               decoration: kTextFieldDecoration.copyWith(
                 labelText: 'Enter your password.',
@@ -57,9 +64,33 @@ class _LoginScreenState extends State<LoginScreen> {
               height: 24.0,
             ),
             AppBtn(
-              onTap: () {
-                //Implement login functionality.
-                Navigator.pop(context);
+              onTap: () async {
+                try {
+                  final loggedIn = await _auth.signInWithEmailAndPassword(
+                    email: email.trim(),
+                    password: passwd.trim(),
+                  );
+
+                  if (loggedIn != null) {
+                    Navigator.pushNamed(context, ChatScreen.id);
+                  }
+                } on FirebaseException catch (e) {
+                  print(e.code);
+                  if (e.code == 'wrong-password') {
+                    Alert(
+                            context: context,
+                            title: 'Wrong Password',
+                            desc: 'Please! Enter a valid password.')
+                        .show();
+                  }
+                  if (e.code == 'too-many-requests') {
+                    Alert(
+                            context: context,
+                            title: 'Too Many Requests',
+                            desc: '${e.message}')
+                        .show();
+                  }
+                }
               },
               btnTitle: 'Log In',
               btnColor: Colors.lightBlueAccent,

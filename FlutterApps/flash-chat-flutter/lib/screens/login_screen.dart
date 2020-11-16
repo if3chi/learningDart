9528873028118df1bb2c1e89905cbd618f6b4dd5
Jaspar Ifechi <j.cyxtus@gmail.com>
2020-11-16 13:50:14 +0000

    D44-47 Flash Cahp app: Implemmented loading indicator.

--- FlutterApps/flash-chat-flutter/lib/screens/login_screen.dart
+++ FlutterApps/flash-chat-flutter/lib/screens/login_screen.dart
@@ -4,6 +4,7 @@ import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = 'LoginScreen';
@@ -16,86 +17,96 @@ class _LoginScreenState extends State<LoginScreen> {
   FirebaseAuth _auth = FirebaseAuth.instance;
   String email;
   String passwd;
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: isLoading,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (value) {
-                email = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                labelText: 'Enter your email',
+              SizedBox(
+                height: 48.0,
               ),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              obscureText: true,
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                passwd = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                labelText: 'Enter your password.',
+              TextField(
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  labelText: 'Enter your email',
+                ),
               ),
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            AppBtn(
-              onTap: () async {
-                try {
-                  final loggedIn = await _auth.signInWithEmailAndPassword(
-                    email: email.trim(),
-                    password: passwd.trim(),
-                  );
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                obscureText: true,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  passwd = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  labelText: 'Enter your password.',
+                ),
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              AppBtn(
+                onTap: () async {
+                  setState(() {
+                    isLoading = true;
+                  });
+                  try {
+                    final loggedIn = await _auth.signInWithEmailAndPassword(
+                      email: email.trim(),
+                      password: passwd.trim(),
+                    );
 
-                  if (loggedIn != null) {
-                    Navigator.pushNamed(context, ChatScreen.id);
+                    if (loggedIn != null) {
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }
+                  } on FirebaseException catch (e) {
+                    setState(() {
+                      isLoading = false;
+                    });
+                    print(e.code);
+                    if (e.code == 'wrong-password') {
+                      Alert(
+                              context: context,
+                              title: 'Wrong Password',
+                              desc: 'Please! Enter a valid password.')
+                          .show();
+                    }
+                    if (e.code == 'too-many-requests') {
+                      Alert(
+                              context: context,
+                              title: 'Too Many Requests',
+                              desc: '${e.message}')
+                          .show();
+                    }
                   }
-                } on FirebaseException catch (e) {
-                  print(e.code);
-                  if (e.code == 'wrong-password') {
-                    Alert(
-                            context: context,
-                            title: 'Wrong Password',
-                            desc: 'Please! Enter a valid password.')
-                        .show();
-                  }
-                  if (e.code == 'too-many-requests') {
-                    Alert(
-                            context: context,
-                            title: 'Too Many Requests',
-                            desc: '${e.message}')
-                        .show();
-                  }
-                }
-              },
-              btnTitle: 'Log In',
-              btnColor: Colors.lightBlueAccent,
-            )
-          ],
+                },
+                btnTitle: 'Log In',
+                btnColor: Colors.lightBlueAccent,
+              )
+            ],
+          ),
         ),
       ),
     );
--- FlutterApps/flash-chat-flutter/lib/screens/registration_screen.dart
+++ FlutterApps/flash-chat-flutter/lib/screens/registration_screen.dart
@@ -3,6 +3,7 @@ import 'package:flash_chat/constants.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 
 class RegistrationScreen extends StatefulWidget {
@@ -16,77 +17,90 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   String email;
   String password;
   FirebaseAuth _auth = FirebaseAuth.instance;
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: isLoading,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                email = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                labelText: 'Enter your email',
+              SizedBox(
+                height: 48.0,
               ),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              obscureText: true,
-              onChanged: (value) {
-                password = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                labelText: 'Enter your password',
+              TextField(
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  labelText: 'Enter your email',
+                ),
               ),
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            AppBtn(
-              onTap: () async {
-                try {
-                  final newUser = await _auth.createUserWithEmailAndPassword(
-                    email: email.trim(),
-                    password: password.trim(),
-                  );
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                obscureText: true,
+                obscuringCharacter: '#',
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  labelText: 'Enter your password',
+                ),
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              AppBtn(
+                onTap: () async {
+                  setState(() {
+                    isLoading = true;
+                  });
+                  try {
+                    final newUser = await _auth.createUserWithEmailAndPassword(
+                      email: email.trim(),
+                      password: password.trim(),
+                    );
 
-                  if (newUser != null) {
-                    Navigator.pushNamed(context, ChatScreen.id);
+                    if (newUser != null) {
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }
+                  } on FirebaseException catch (e) {
+                    // if(e.code == ){}
+
+                    setState(() {
+                      isLoading = false;
+                    });
+
+                    Alert(
+                      context: context,
+                      title: "",
+                      desc: "${e.message}",
+                      // image: Image.asset("assets/success.png"),
+                    ).show();
                   }
-                } on FirebaseException catch (e) {
-                  // if(e.code == ){}
-                  Alert(
-                    context: context,
-                    title: "",
-                    desc: "${e.message}",
-                    // image: Image.asset("assets/success.png"),
-                  ).show();
-                }
-              },
-              btnColor: Colors.blueAccent,
-              btnTitle: 'Register',
-            ),
-          ],
+                },
+                btnColor: Colors.blueAccent,
+                btnTitle: 'Register',
+              ),
+            ],
+          ),
         ),
       ),
     );

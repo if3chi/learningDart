3476c62beda3f7a4d6ca776b45972cafafc4e977
Jaspar Ifechi <j.cyxtus@gmail.com>
2020-11-24 21:14:11 +0000

    D52-55 Todo App: Implemented the checkbox fuction using callbacks.

--- FlutterApps/todoey/lib/components/list_card_widget.dart
+++ FlutterApps/todoey/lib/components/list_card_widget.dart
@@ -15,26 +15,51 @@ class ListViewWidget extends StatelessWidget {
   }
 }
 
-class ListTileCard extends StatelessWidget {
+class ListTileCard extends StatefulWidget {
+  @override
+  _ListTileCardState createState() => _ListTileCardState();
+}
+
+class _ListTileCardState extends State<ListTileCard> {
+  bool isChecked = false;
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
         '1. Task',
         style: TextStyle(
-          color: Colors.black45,
+          color: Colors.black,
           fontSize: 35,
+          decoration: isChecked ? TextDecoration.lineThrough : null,
         ),
       ),
-      trailing: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Checkbox(
-          value: false,
+      trailing: TodoCheckBox(
+        isChecked: isChecked,
+        todoCheck: (value) {
+          setState(() {
+            isChecked = value;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class TodoCheckBox extends StatelessWidget {
+  TodoCheckBox({this.isChecked, this.todoCheck});
+
+  final bool isChecked;
+  final Function todoCheck;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Checkbox(
+          value: isChecked,
           checkColor: Colors.white,
           activeColor: kRedRoseLight,
-          onChanged: (value) {},
-        ),
-      ),
+          onChanged: todoCheck),
     );
   }
 }
